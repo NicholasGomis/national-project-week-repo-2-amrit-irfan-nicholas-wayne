11df503a03986b5fcb72f35a00a7219779007840
NicholasGomis <nicholasgomis@icloud.com>
2022-01-18 13:11:16 +0000

    Added some css for post and form

--- codebuddy/src/components/Form/Form.js
+++ codebuddy/src/components/Form/Form.js
@@ -6,10 +6,15 @@ export function Form(props) {
     return (
         <form className="form">
 
+         <label for="insert-name">Name:</label>
         <input className="name-input" placeholder='Insert your name'></input>
+         <label for="insert-title">Title:</label>
         <input className ="title-input" placeholder='Insert post title'></input>
+         <label for="insert-describe">Describe:</label>
         <input className="problem-input" placeholder='Explain problem'></input>
+         <label for="insert-attempt">Attempt:</label>
         <input className="tried-input" placeholder='What have you tried'></input>
+        <label for="insert-code">Code:</label>
         <input className="code-input" placeholder='Insert your code here'></input>
         <input type="submit" />
         </form>
