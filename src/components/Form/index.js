a75d47916661a519f149b15e9fec74b05883a65c
waynethrower <wayne.thrower@hotmail.co.uk>
2022-01-19 16:47:28 +0000

    Added async function to Post form data

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState } from "react";
 
+//declare vars
+const API_URL = "https://code-review-soc-app.herokuapp.com";
+
 // add state to update posts
 // async Create function to send the new post to the api /post
 // response from the api new list of posts
@@ -54,7 +57,15 @@ export function Form() {
 
   function addPost(inputName, inputTitle, inputProblem, inputTried, inputCode) {
     console.log("submit received");
-    console.log(inputName, inputTitle, inputProblem, inputTried, inputCode);
+    const post = {
+      auth_id: 1,
+      title: inputTitle,
+      problem: inputProblem,
+      attempted: inputTried,
+      content: inputCode,
+    };
+    console.log(post);
+    createPosts(post);
     updateName("");
     updateTitle("");
     updateProblem("");
@@ -62,83 +73,95 @@ export function Form() {
     updateCode("");
   }
 
-  //dont send: auth id: num //date
-
-  //title
-  //input problem
-  //input attempted
-  //code
-
-  // api_name/posts/create
-
-  //send:
-  //date
-  //title
-  //input problem
-  //input attempted
-  //code
-
-  // api_name/posts/create
-
-  // add state to update posts
-  // async Create function to send the new post to the api /post
-  // response from the api new list of posts
-
-  // /posts
-
-  //action=""
+  // application/json for line 80
+  async function createPosts(post) {
+    const response = await fetch(`${API_URL}/posts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: JSON.stringify(post),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
-
     <form className="form" onSubmit={handleSubmit}>
       <div className="title">What is your question?</div>
       <div className="subtitle">Articulate your question in the soc way!</div>
       <div className="input-container ic1">
-        <input id="firstname" className="input" type="text" placeholder=" " value={inputName} onChange={handleNameChange}/>
+        <input
+          id="firstname"
+          className="input"
+          type="text"
+          placeholder=" "
+          value={inputName}
+          onChange={handleNameChange}
+        />
         <div className="cut"></div>
         <label htmlFor="firstname" className="placeholder">
           Name
         </label>
       </div>
       <div className="input-container ic2">
-        <input id="title" className="input" type="text" placeholder=" " value={inputTitle}
-        onChange={handleTitleChange}/>
+        <input
+          id="title"
+          className="input"
+          type="text"
+          placeholder=" "
+          value={inputTitle}
+          onChange={handleTitleChange}
+        />
         <div className="cut"></div>
         <label htmlFor="title" className="placeholder">
           Title
         </label>
       </div>
       <div className="input-container ic2">
-        <input id="describe" className="input" type="text" placeholder=" " value={inputProblem}
-        onChange={handleProblem} />
+        <input
+          id="describe"
+          className="input"
+          type="text"
+          placeholder=" "
+          value={inputProblem}
+          onChange={handleProblem}
+        />
         <div className="cut cut-short"></div>
         <label htmlFor="describe" className="placeholder">
           Describe your problem
         </label>
       </div>
       <div className="input-container ic2">
-        <input id="attempted" className="input" type="text" placeholder=" "  value={inputTried}
-        onChange={handleTried}/>
+        <input
+          id="attempted"
+          className="input"
+          type="text"
+          placeholder=" "
+          value={inputTried}
+          onChange={handleTried}
+        />
         <div className="cut cut-short"></div>
         <label htmlFor="describe" className="placeholder">
           What have you tried?
         </label>
       </div>
       <div className="input-container-new ic2">
-        <input id="code" className="input" type="text" placeholder=" "  value={inputCode}
-        onChange={handleCode}/>
+        <input
+          id="code"
+          className="input"
+          type="text"
+          placeholder=" "
+          value={inputCode}
+          onChange={handleCode}
+        />
         <div className="cut cut-short"></div>
         <label htmlFor="code" className="placeholder">
           Insert your code here
         </label>
       </div>
-      <button type="text" className="submit">
+      <button type="submit" className="submit">
         submit
       </button>
-    </div>
-
-  
-
+    </form>
   );
 }
 
